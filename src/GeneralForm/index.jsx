f0b594901f52e13929fac9ecc6b4bc5ab1fb10bc
Tyler Collingridge <tylercollingridge@gmail.com>
2024-07-30 15:01:07 -0600

    feat: submit functionality for general info form
    - Generate div that displays general info.
    - Create edit button to display form again.
    - Add styling to submitted form.

--- src/GeneralForm/index.jsx
+++ src/GeneralForm/index.jsx
@@ -1,23 +1,84 @@
+import { useState } from "react";
+
 function GeneralInfoForm() {
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [generalInfo, setGeneralInfo] = useState({
+    name: "",
+    email: "",
+    phone: "",
+  });
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    const { name, email, phone } = generalInfo;
+
+    if (name && email && phone) {
+      setIsSubmitted(true);
+    } else {
+      alert("Please fill out all fields.");
+    }
+  };
+
+  const handleInputChange = (event) => {
+    const { id, value } = event.target;
+    setGeneralInfo((prevInfo) => ({ ...prevInfo, [id]: value }));
+  };
+
+  const handleEditClick = () => {
+    setIsSubmitted(false);
+  };
+
   return (
-    <div id="gen-info">
-      <form action="">
-        <legend>General Information</legend>
-        <label htmlFor="name">Name</label>
-        <input type="text" id="name" required />
-        <label htmlFor="email">Email</label>
-        <input type="email" id="email" required />
-        <label htmlFor="phone">Phone Number</label>
-        <input
-          type="tel"
-          id="phone"
-          name="phone"
-          pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-          required
-        />
-        <button type="submit">Submit</button>
-      </form>
-    </div>
+    <>
+      <h2>General Info</h2>
+      {!isSubmitted ? (
+        <form action="" onSubmit={handleFormSubmit}>
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            id="name"
+            required
+            value={generalInfo.name}
+            onChange={handleInputChange}
+          />
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            required
+            value={generalInfo.email}
+            onChange={handleInputChange}
+          />
+          <label htmlFor="phone">Phone Number</label>
+          <input
+            type="tel"
+            id="phone"
+            name="phone"
+            pattern="[0-9]{10}"
+            required
+            value={generalInfo.phone}
+            onChange={handleInputChange}
+          />
+          <button type="submit">Submit</button>
+        </form>
+      ) : (
+        <div className="general info">
+          <p>
+            <strong>Name:</strong> {generalInfo.name}
+          </p>
+          <p>
+            <strong>Email:</strong> {generalInfo.email}
+          </p>
+          <p>
+            <strong>Phone:</strong> {generalInfo.phone}
+          </p>
+          <button id="edit" onClick={handleEditClick}>
+            Edit
+          </button>
+        </div>
+      )}
+    </>
   );
 }
 
